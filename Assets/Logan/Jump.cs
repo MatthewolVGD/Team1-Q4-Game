e4d3a2b7f19837ba0f69f1059e30fbe5c4449d56
Logan Chriscaden <Baconlad123@gmail.com>
2021-04-16 09:57:39 -0700

    Tried to get player working, didn't work

---
+++ Assets/Logan/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpStrength = 400;
+    public bool grounded;
+    private Rigidbody2D rb2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int layerMask = 1 << 9;
+        layerMask = ~layerMask;
+
+        float DistanceToTheGround = GetComponent<BoxCollider2D>().bounds.extents.y;
+        grounded = Physics2D.Raycast(transform.position, Vector2.down, DistanceToTheGround + 0.01f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.blue);
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb2.AddForce(new Vector2(0, jumpStrength));
+        }
+    }
+}
