c46a0a56452da9915c56137e0cae08058c2f9221
MatthewolVGD <1707240@mukilteo.wednet.edu>
2021-04-23 15:10:38 -0700

    Split skeleton and charger scripts

--- Assets/Matthew/Scripts/ChargeEnemyScript.cs
+++ Assets/Matthew/Scripts/ChargeEnemyScript.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class ChargeEnemyScript : MonoBehaviour
 {
-
     #region MainVariables
     public float speed;//Enemy Speed
     public int health;//Enemy Health
     public float jumpStrength;//Enemy Jump Velocity
     public int damage;//Enemy Damage
     public float attackTimer;//Time between attacks
-    public float attackDist;//How close the enemy has to be to attack the player
     float ogAttackTimer;//Tracking original attack time for reset purposes
     public Transform player;//The player
     Rigidbody2D rb;//Enemy's rigidbody
     public float chasePlayerDist;//How close the player has to be to chase them
     public int detAdd;//Amount of determination added to player after this enemy dies
+    public float chargerAtkDist;//How close the player has to be for a charging enemy to attack them
+    public float chargeSpeed;//Speed enemy charges at
+    public bool charging;//Whether enemy is charging
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -28,38 +29,33 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, player.transform.position) < chasePlayerDist)//Only chase player if they're within this distance
+        if (Vector2.Distance(transform.position, player.transform.position) < chasePlayerDist && !charging)//Only chase player if they're within this distance
         {
             Movement();
         }
 
-        attackTimer -= Time.deltaTime;
-        if(Vector2.Distance(transform.position, player.transform.position) < attackDist && attackTimer <= 0f)//Only attack player if they're within this distance, skeleton
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) <= chargerAtkDist && attackTimer <= 0f)//Only attack player if they're within this distance, charger
         {
-            Attack();
+            StartCoroutine(ChargeAttack());
             attackTimer = ogAttackTimer;
         }
-
-        
     }
 
     void Movement()//Handles all enemy movement
     {
         
-        
-            if (transform.position.x - player.position.x < -3f)//Player to right
+            if (transform.position.x - player.position.x < -6f)//Player to right
             {
                 rb.velocity = new Vector2(speed, rb.velocity.y);
             }
-            else if (transform.position.x - player.position.x > 3f)//Player to left
+            else if (transform.position.x - player.position.x > 6f)//Player to left
             {
                 rb.velocity = new Vector2(-speed, rb.velocity.y);
             }
-            else if(transform.position.x - player.position.x >= -3f || transform.position.x - player.position.x <= 3f)
+            else if (transform.position.x - player.position.x >= -6f || transform.position.x - player.position.x <= 6f)
             {
                 rb.velocity = new Vector2(0, rb.velocity.y);
             }
-        
 
 
         int layerMask = 1 << 8;
@@ -67,11 +63,11 @@ public class EnemyScript : MonoBehaviour
 
         float DistanceToTheGround = GetComponent<BoxCollider2D>().bounds.extents.y;
         Ray enemyRay = new Ray(transform.position - new Vector3(0, DistanceToTheGround, 0), Vector2.down);
-        RaycastHit2D grounded = Physics2D.Raycast(transform.position - new Vector3(0, DistanceToTheGround + 0.01f,0), Vector2.down, 0.01f);
+        RaycastHit2D grounded = Physics2D.Raycast(transform.position - new Vector3(0, DistanceToTheGround + 0.01f, 0), Vector2.down, 0.01f);
         Debug.DrawRay(transform.position + new Vector3(0, -DistanceToTheGround, 0), Vector2.down, Color.blue);
         RaycastHit2D wallToRight = Physics2D.Raycast(transform.position, Vector2.right, 4f, layerMask);
         RaycastHit2D wallToLeft = Physics2D.Raycast(transform.position, -Vector2.right, 4f, layerMask);
-        if(grounded.collider != null)
+        if (grounded.collider != null)
         {
             if (player.transform.position.y > transform.position.y + 1f && grounded.collider.gameObject.tag == "Terrain")
             {
@@ -83,17 +79,34 @@ public class EnemyScript : MonoBehaviour
                         rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
                     }
                 }
-                
+
 
             }
         }
-        
-        
+
+
     }
 
-    void Attack()//Handles enemy attack
+    IEnumerator ChargeAttack()
     {
-        player.gameObject.GetComponent<Player>().Damage(damage);
+        charging = true;
+        Debug.Log("Charging");
+        gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log("Moving");
+        if (transform.position.x - player.position.x < -0.1f)//Player to right
+        {
+            rb.velocity = new Vector2(chargeSpeed, rb.velocity.y);
+        }
+        else if (transform.position.x - player.position.x > 0.1f)//Player to left
+        {
+            rb.velocity = new Vector2(-chargeSpeed, rb.velocity.y);
+        }
+        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        yield return new WaitForSeconds(3f);
+        charging = false;
+        Debug.Log("Stop charging");
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
     }
 
     public void Damage(int damage, GameObject dealer)//Handles enemy taking damage
@@ -106,4 +119,11 @@ public class EnemyScript : MonoBehaviour
         dealer.gameObject.GetComponent<Player>();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (charging && collision.gameObject.tag == "Player")
+        {
+            player.gameObject.GetComponent<Player>().Damage(damage);
+        }
+    }
 }
